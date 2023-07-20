0f2bdf22e781589978fd0b0233a2d6e21e746fb1
roniekun <roniebenitez01@gmail.com>
2023-07-20 16:51:02 +0800

    initial commit

--- src/components/main/sections/HomeContent.jsx
+++ src/components/main/sections/HomeContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './HomeContent.css';
 import ObjectParallax from '../../../assets/Parallax/ObjectParallax';
+import Logo from '../../../assets/Temp/Logo';
 
 
 const HomeContent = () => {
@@ -8,6 +9,7 @@ const HomeContent = () => {
   return (
     <div className='home__content__container'>
      <ObjectParallax/>
+     <Logo/>
 
     </div>
   )
