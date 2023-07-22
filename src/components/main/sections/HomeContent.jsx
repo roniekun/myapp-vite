a1aad570885ec1d3f7410f07f8c9e62c0ec58068
roniekun <roniebenitez01@gmail.com>
2023-07-22 22:45:09 +0800

    commit

--- src/components/main/sections/HomeContent.jsx
+++ src/components/main/sections/HomeContent.jsx
@@ -1,27 +1,19 @@
 import React from 'react'
 import './HomeContent.css';
-import ObjectParallax from '../../../assets/Parallax/ObjectParallax';
+// import ObjectParallax from '../../../assets/Parallax/ObjectParallax';
 // import Logo from '../../../assets/Temp/Logo';
 // import PscLogo from '../../../assets/Temp/PscLogo';
 import ImageSlider from './elements/imageslider/ImageSlider';
 
 
 
-const containerStyles = {
-  width: "100vw",
-  height: "100vh",
-  margin: "0 auto",
-};
-
-
 const HomeContent = () => {
 
   return (
     <div className='home__content__container'>
-     <ObjectParallax/>
-     <div style={containerStyles}>
+     {/* <ObjectParallax/> */}
         <ImageSlider />
-      </div>
+
 
     </div>
   )
--- src/components/main/sections/elements/imageslider/ImageSlider.jsx
+++ src/components/main/sections/elements/imageslider/ImageSlider.jsx
@@ -1,75 +1,32 @@
-import { useState, useEffect } from "react";
-import { slides } from './ImageSliderData'; 
-
-const slideStyles = {
-  width: "100%",
-  height: "100%",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
- 
-};
-
-const rightArrowStyles = {
-  display:'flex',
-  position: "relative",
-  height: 'fit-content',
-  fontSize: "45px",
-  color: "#fff",
-  zIndex: 1,
-  cursor: "pointer",
-  width: 'fit-content'
-};
-
-const leftArrowStyles = {
-  position: "relative",
-  height: 'fit-content',
-  fontSize: "45px",
-  color: "#fff",
-  zIndex: 1,
-  cursor: "pointer",
-  width: 'fit-content'
-};
-
-const sliderStyles = {
-  position: "relative",
-  height: "100%",
-};
-
-const dotsContainerStyles = {
-  display: "flex",
-  justifyContent: "center",
-  zIndex: '1',
-  position: 'absolute',
-  width: '100vw',
-  bottom: 0,
-  alignItmes: 'center',
-  backgroundColor: 'transparent'
-  
-
-};
+import React, { useState, useEffect } from "react";
+import { slides } from './ImageSliderData';
+import './ImageSlider.css';
 
 const ImageSlider = () => {
-
   const [currentIndex, setCurrentIndex] = useState(0);
+
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
+
   const goToNext = () => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
+
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
   };
 
-  
   const slideStylesWidthBackground = {
-    ...slideStyles,
+    width: "100vh",
+    height: "100vh",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
     backgroundImage: `url(${slides[currentIndex].url})`,
-    // transition: "background-image 0.5s ease",
   };
 
   const goToNextSlide = () => {
@@ -78,64 +35,32 @@ const ImageSlider = () => {
     setCurrentIndex(newIndex);
   };
 
-  // useEffect to automatically transition to the next slide after 3 seconds
+  // useEffect to automatically transition to the next slide after 5 seconds (5000ms)
   useEffect(() => {
-    const intervalId = setInterval(goToNextSlide, 5000); // 3000ms = 3 seconds
+    const intervalId = setInterval(goToNextSlide, 5000); // 5000ms = 5 seconds
 
     // Clean up the interval when the component is unmounted or currentIndex changes
     return () => clearInterval(intervalId);
   }, [currentIndex]);
 
   return (
-    <div style={sliderStyles}>
-     {/* <div style={dotsContainerStyles}>
 
-    
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            style={{cursor: 'pointer', color:'white'}}
-          >
-            ●
-          </div>
-        ))}
-      </div> */}
- <div style={{
-            position:'absolute',
-            height: '100%',
-            left:'10px',
-            width: '2%',
-            display: 'flex',
-            alignItems:'center',
-            justifyContent: 'center'
+    <div className='slider'>
 
-
-}}>
-      <div onClick={goToPrevious} style={leftArrowStyles}>
-    ❰
-       </div>
-</div>
-
-<div style={{
-            position:'absolute',
-            height: '100%',
-            right:'10px',
-            width: '2%',
-            display: 'flex',
-            alignItems:'center',
-            justifyContent: 'center'
-
-
-}}>
-       <div onClick={goToNext} style={rightArrowStyles}>
-    ❱
-       </div>
-</div>
-
-    <div style={slideStylesWidthBackground}>
-
+      <div className='left__arrow__container'
+      >
+        <div onClick={goToPrevious} className='left__arrow'>
+         &#8249;
+        </div>
       </div>
+
+      <div className='right__arrow__container'>
+        <div onClick={goToNext} className="right__arrow">
+        &#8250;
+        </div>
+      </div>
+
+      <div style={slideStylesWidthBackground}></div>
     </div>
   );
 };
