77e3477cc8aff5cc24335f0f3325849bc7b4cb4e
roniekun <roniebenitez01@gmail.com>
2023-07-28 11:20:19 +0800

    update

--- src/components/main/sections/AboutContent.jsx
+++ src/components/main/sections/AboutContent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './AboutContent.css'
-import ReadMoreButton from '../../buttons/ReadMoreButton';
+// import ReadMoreButton from '../../buttons/ReadMoreButton';
 import { ThemeContext } from '../../../context/ThemeContext';
 
 const AboutContent = () => {
@@ -16,17 +16,19 @@ const AboutContent = () => {
     <div  id={`component-${theme}`} className='about__content__container'>
         <section className='about__first__section'>
 
-        <div className= {isClicked ? 'about__text__container__clicked' : 'about__text__container' }>
+        {/* <div className= {isClicked ? 'about__text__container__clicked' : 'about__text__container' }> */}
+
+        <div className='about__text__container'>
                        <p > A self-taught web developer with passion for creating stunning and functional websites. 
                         I enjoy crafting captivating webpage, strive to create visually appealing and intuitive interfaces that engage users and convey the essence of a brand or concept. 
                         Driven by a desire for constant growth, I challenge myself to learn and adapt to the ever-evolving world of web development.
                         Every day, I dedicate time to refine my coding abilities and explore new techniques, ensuring that I stay at the forefront of industry trends.
-                        </p>
+                        </p><br/>
                         <p>
                         As I continue to expand my expertise, my goal is to become a well-rounded full-stack developer.
                         I'm eager to explore back-end technologies, databases, and server management,
                         so I can create end-to-end solutions that provide seamless functionality and an exceptional user experience.
-                        </p>
+                        </p><br/>
                         <p>
                         I believe in the power of collaboration and actively seek opportunities to work with like-minded individuals and contribute to open-source projects.
                         Through collaboration and sharing knowledge, I aim to foster a supportive community that drives innovation and pushes the boundaries of what is possible.
@@ -37,8 +39,8 @@ const AboutContent = () => {
 
 
         </div>
-        <ReadMoreButton onClick={handleClick}
-                       isClicked={isClicked}/>
+        {/* <ReadMoreButton onClick={handleClick}
+                       isClicked={isClicked}/> */}
         </section>
 
     </div>
